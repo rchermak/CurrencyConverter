4a384f0a0591f368e7e6e206c9fd99879f922d44
Rachael Chermak <rachaelchermak@Rachaels-MBP.Home>
2021-02-28 23:37:26 -0600

    Initial commit

---
+++ app/src/main/java/css/currencyconverter/MainActivity.java
@@ -0,0 +1,48 @@
+package css.currencyconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editTextDollars;
+    EditText editTextEuros;
+    Button buttonConvertToDollars;
+    Button buttonConvertToEuros;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextDollars = findViewById(R.id.editTextDollars);
+        editTextEuros = findViewById(R.id.editTextEuros);
+
+        buttonConvertToEuros.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String dollarString = editTextDollars.getText().toString();
+                Double dollars = Double.parseDouble(dollarString);
+                Double euros = dollars * 0.83;
+                editTextEuros.setText(NumberFormat.getCurrencyInstance().format( euros ));
+            }
+        });
+
+        buttonConvertToDollars.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String euroString = editTextEuros.getText().toString();
+                Double euros = Double.parseDouble(euroString);
+                Double dollars = euros * 1.21;
+                editTextDollars.setText(NumberFormat.getCurrencyInstance().format( dollars ));
+            }
+        });
+
+    }
+}
